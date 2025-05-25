3f88ff0ae08179d47ff5cc17c43ed6df3d788df0
MakSimKlim <83maxomus@mail.ru>
2025-05-25 16:49:51 +0500

    Add A Default Role to Newly Registered User DONE

--- DiplomaProjectTopAcademy/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DiplomaProjectTopAcademy/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -161,8 +161,9 @@ namespace DiplomaProjectTopAcademy.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-
-                    //await _userManager.AddToRoleAsync(user, Enums.Roles.Basic.ToString());//Add A Default Role to Newly Registered User
+                    //Add A Default Role to Newly Registered User - Inactive:
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Inactive.ToString());
+                    
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
